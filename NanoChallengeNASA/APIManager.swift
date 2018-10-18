e3e0d37b779fe26bc8e05bd436cd205f06bfc6e5
Mateus Rodrigues <mateus.fr26@gmail.com>
2018-10-18 15:03:58 -0300

    Add APIManager class and Response struct

---
+++ NanoChallengeNASA/APIManager.swift
@@ -0,0 +1,78 @@
+//
+//  APIManager.swift
+//  NanoChallengeNASA
+//
+//  Created by Mateus Rodrigues on 18/10/18.
+//  Copyright © 2018 Mateus Rodrigues. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class APIManager {
+    
+    let imageCache = NSCache<NSString, UIImage>()
+    
+    static let shared = APIManager()
+    
+    func searchImage(query: String, completion: @escaping ([Image]?, Error?) -> Void) {
+        
+        let url = URL(string: "https://images-api.nasa.gov/search?q=\(query)&media_type=image")!
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data, error == nil else {
+                completion(nil, error)
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(Response.self, from: data)
+                self.downloadMetadata(images: response.items, completion: { (images, error) in
+                    if (error != nil) {
+                        completion(nil, error)
+                    } else {
+                        completion(images, error)
+                    }
+                })
+                
+                
+                
+            } catch {
+                completion(nil, error)
+            }
+            }.resume()
+    }
+    
+    
+    private func downloadMetadata(images: [Image], completion: @escaping ([Image]?, Error?) -> Void) {
+        
+        let group = DispatchGroup()
+        
+        var images = images
+        
+        for (index, image) in images.enumerated() {
+            let id = (image.id)!
+            let url = URL(string: "https://images-assets.nasa.gov/image/\(id)/metadata.json")!
+            
+            group.enter()
+            
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                do {
+                    let decoder = JSONDecoder()
+                    let metadata = try decoder.decode(Metadata.self, from: data!)
+                    images[index].setSize(width: metadata.imageWidth, height: metadata.imageHeight)
+                } catch {
+                    print(error)
+                }
+                group.leave()
+                }.resume()
+            
+        }
+        
+        group.notify(queue: .main) {
+            print("Finished all requests.")
+            completion(images, nil)
+        }
+    }
+    
+}
---
+++ NanoChallengeNASA/Response.swift
@@ -0,0 +1,87 @@
+//
+//  Response.swift
+//  NanoChallengeNASA
+//
+//  Created by Mateus Rodrigues on 18/10/18.
+//  Copyright © 2018 Mateus Rodrigues. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Response: Decodable {
+    var version: String
+    var items = [Image]()
+    
+    enum CodingKeys: String, CodingKey {
+        case collection
+    }
+    
+    enum CollectionKeys: String, CodingKey {
+        case items, version
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let collection = try container.nestedContainer(keyedBy: CollectionKeys.self, forKey: .collection)
+        self.version = try collection.decode(String.self, forKey: .version)
+        self.items = try collection.decode([Image].self, forKey: .items)
+    }
+}
+
+struct Image: Decodable {
+    
+    var href: String?
+    var id: String?
+    var title: String?
+    var description: String?
+    var thumbUrl: URL?
+    var imageSize: CGSize?
+    
+    enum CodingKeys: String, CodingKey {
+        case links, data
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let data = try container.decode([ImageData].self, forKey: .data)
+        let links = try container.decode([ImageLink].self, forKey: .links)
+        
+        self.id = data.first?.nasa_id
+        self.title = data.first?.title
+        self.description = data.first?.description
+        self.thumbUrl = URL(string: (links.first?.href)!)
+    }
+    
+    mutating func setSize(width: Int?, height: Int?) {
+        guard let width = width, let height = height else {
+            self.imageSize = nil
+            return
+        }
+        self.imageSize = CGSize(width: width, height: height)
+    }
+    
+}
+
+struct ImageData: Decodable {
+    var description: String
+    var title: String
+    var nasa_id: String
+}
+
+struct ImageLink: Decodable {
+    var href: String
+}
+
+struct Metadata: Decodable {
+    
+    var imageHeight: Int?
+    var imageWidth: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case imageHeight = "File:ImageHeight"
+        case imageWidth = "File:ImageWidth"
+    }
+    
+}
